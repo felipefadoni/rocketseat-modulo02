5a9218ded16bf43c57194bef3278c1b93d5caad1
Felipe <felipe.fadoni@epta.com.br>
2019-10-15 18:01:19 -0300

    criacao migrations, configuracao database, models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Felipe' });
+routes.get('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: 'Felipe Fadoni',
+      email: 'felipe@webfadoni.com',
+      password_hash: '123123123',
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.json({ error: `O dado ${e} já estão cadastrado`, code: e });
+  }
 });
 
 export default routes;
