1ffbec41d98cb6c00346961a5aa2a870b36675a4
Felipe <felipe.fadoni@epta.com.br>
2019-10-20 10:41:42 -0300

    update gitignore

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '239b2d69bc7e4b',
+    pass: '171fb317456116',
+  },
+  default: {
+    from: 'Felipe Fadoni <felipe@webfadoni.com>',
+  },
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://e684520ba0144e21bfb54f2aa1714702@sentry.io/1785052',
+};
